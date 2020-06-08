8b1cdc33143275b22a2cbdaa7daa03a47148f58e
John Lopez <johnlopez@Johns-MacBook-Pro.local>
2020-06-08 18:48:54 -0500

    updates

--- app/bot/views.py
+++ app/bot/views.py
@@ -3,6 +3,7 @@ import requests
 import json
 from flask import Blueprint, current_app, request
 from twilio.twiml.messaging_response import MessagingResponse
+from twilio import twiml
 from .util import detect_intent_texts
 
 blueprint = Blueprint('bot', __name__, url_prefix='/bot')
