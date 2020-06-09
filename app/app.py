1f7ff9e162f1c2f24a5e6e2919531bf85e99bee6
John Lopez <johnlopez@Johns-MacBook-Pro.local>
2020-06-08 20:00:57 -0500

    updates

--- app/app.py
+++ app/app.py
@@ -50,6 +50,7 @@ def register_extensions(app):
 def register_blueprints(app):
     """Register Flask blueprints."""
     app.register_blueprint(public.views.blueprint)
+    csrf_protect.exempt(public.views.blueprint)
     app.register_blueprint(user.views.blueprint)
     app.register_blueprint(bot.views.blueprint)
     return None
--- app/bot/views.py
+++ app/bot/views.py
@@ -1,15 +1,38 @@
 import logging
-import requests
 import json
 from flask import Blueprint, current_app, request
+import requests
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio import twiml
 from .util import detect_intent_texts
+from app.extensions import csrf_protect
 
 blueprint = Blueprint('bot', __name__, url_prefix='/bot')
 
 #logging.getLogger('flask_assistant').setLevel(logging.DEBUG)
 
 @blueprint.route('/twilio', methods=['POST'])
+@csrf_protect.exempt
 def twilio():
-    return "123"
+    incoming_msg = request.values.get('Body', '').lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+    if 'quote' in incoming_msg:
+        # return a quote
+        r = requests.get('https://api.quotable.io/random')
+        if r.status_code == 200:
+            data = r.json()
+            quote = f'{data["content"]} ({data["author"]})'
+        else:
+            quote = 'I could not retrieve a quote at this time, sorry.'
+        msg.body(quote)
+        responded = True
+    if 'cat' in incoming_msg:
+        # return a cat pic
+        msg.media('https://cataas.com/cat')
+        responded = True
+    if not responded:
+        msg.body('I only know about famous quotes and cats, sorry!')
+    print(str(resp))
+    return str(resp)
