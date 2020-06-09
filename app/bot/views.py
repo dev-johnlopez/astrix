0129a6870ee040054d4a7c87cd0b89212024ace3
John Lopez <johnlopez@Johns-MacBook-Pro.local>
2020-06-08 19:11:11 -0500

    updates

--- app/bot/views.py
+++ app/bot/views.py
@@ -12,25 +12,24 @@ blueprint = Blueprint('bot', __name__, url_prefix='/bot')
 
 @blueprint.route('/twilio', methods=['POST'])
 def twilio():
-    resp = twiml.Response()
-    resp.message('Hello, you said: nothing')
+    incoming_msg = request.values.get('Body', '').lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+    if 'quote' in incoming_msg:
+        # return a quote
+        r = requests.get('https://api.quotable.io/random')
+        if r.status_code == 200:
+            data = r.json()
+            quote = f'{data["content"]} ({data["author"]})'
+        else:
+            quote = 'I could not retrieve a quote at this time, sorry.'
+        msg.body(quote)
+        responded = True
+    if 'cat' in incoming_msg:
+        # return a cat pic
+        msg.media('https://cataas.com/cat')
+        responded = True
+    if not responded:
+        msg.body('I only know about famous quotes and cats, sorry!')
     return str(resp)
-    #print("=======")
-    #print("=======")
-    #print("=======")
-    #print("=======")
-    #print("=======")
-    #incoming_msg = request.values.get('Body', '').lower()
-    #print("=======")
-    #print(str(request.values))
-    #print("=======")
-    #resp = MessagingResponse()
-    #msg = resp.message()
-    #responded = False
-    #intent = detect_intent_texts(project_id="autumn-wkhetw",
-    #                               session_id="123",
-    #                               texts=[incoming_msg],
-    #                               language_code="en-US")
-    #msg.body(intent.query_result.fulfillment_text)
-    #responded = True
-    #return str(resp)
